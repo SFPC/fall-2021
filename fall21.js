3bcb581458e23ac8949e94fdec09727b7bb116a0
Zainab Aliyu <zoaliyu@gmail.com>
2021-10-03 14:25:06 -0400

    bg

--- fall21.js
+++ fall21.js
@@ -1,11 +1,18 @@
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
 $(window).scroll(function(){
   var scrollTop = $(window).scrollTop();
-  if(scrollTop < 200){
-    maxHeight = 150;
-  }else if(scrollTop > 400){
-    maxHeight = 300;
-  }else{
-    maxHeight = 300 - 75 * (((scrollTop-300) * 100)/200)/100;
-  }
-  $('.portal').stop().animate({'max-height': maxHeight+"vh"}, 500);
+  var pageHeight = $(document).height();
+
+  var scaleScoll = scale (scrollTop, 0, pageHeight, 1, 5);
+  var scaleOpacity = scale (scrollTop, 0, pageHeight, 1, .3);
+
+    maxHeight = 150 - 75 * (((scrollTop-200) * 100)/200)/100;
+
+  $('.portal').css({transform: 'scale('+scaleScoll+')'});
+
+  $('.portal').css({opacity: scaleOpacity});
+
 })
